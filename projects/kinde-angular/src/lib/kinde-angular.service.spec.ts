374db0e19a77ceff293686c9d289a2aff8c2857a
Luuk Haijes <luuk_haijes@yahoo.com>
2023-12-30 20:36:32 +0100

    feat: add unit tests for kinde-angular.service.spec.ts

--- projects/kinde-angular/src/lib/kinde-angular.service.spec.ts
+++ projects/kinde-angular/src/lib/kinde-angular.service.spec.ts
@@ -1,16 +1,119 @@
 import { TestBed } from '@angular/core/testing';
-
 import { KindeAngularService } from './kinde-angular.service';
+import { KindeClient } from './interfaces/kinde-client.interface';
+import { AuthStateService } from './auth-state.service';
+import { UserType } from '@kinde-oss/kinde-typescript-sdk';
+import { KINDE_FACTORY_TOKEN } from "./kinde-client-factory.service";
+import { LOCATION_TOKEN } from "./tokens/location.token";
 
 describe('KindeAngularService', () => {
-  let service: KindeAngularService;
+  let kindeClientMock: KindeClient;
+  let locationSpy: jasmine.SpyObj<Location>;
+  const createService = () => TestBed.inject(KindeAngularService);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(KindeAngularService);
+    TestBed.resetTestingModule();
+
+    kindeClientMock = {
+      isAuthenticated: () => Promise.resolve(true),
+      getUser: () => Promise.resolve({ family_name: 'test' } as UserType),
+      getToken: () => Promise.resolve('test'),
+      login: () => Promise.resolve(new URL('https://kinde.com/v2/login')),
+      logout: () => Promise.resolve(new URL('https://kinde.com/v2/logout')),
+      handleRedirectToApp: jasmine.createSpy('handleRedirectToApp').and.resolveTo()
+    } as unknown as KindeClient;
+
+    locationSpy = jasmine.createSpyObj('LOCATION_TOKEN', {
+      href: jasmine.createSpy(),
+      search: jasmine.createSpy(),
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: KINDE_FACTORY_TOKEN,
+          useValue: kindeClientMock
+        },
+        {
+          provide: LOCATION_TOKEN,
+          useValue: locationSpy
+        },
+        KindeAngularService,
+        AuthStateService
+      ]
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get user$ correctly', (done) => {
+    const service = createService();
+    service.user$.subscribe(user => {
+      expect(user).toEqual({ family_name: 'test' } as UserType);
+      done();
+    });
+  });
+
+  it('should get isAuthenticated$ correctly', (done) => {
+    const service = createService();
+    service.isAuthenticated$.subscribe(isAuthenticated => {
+      expect(isAuthenticated).toBe(true);
+      done();
+    });
+  });
+
+  it('should get isLoading$ correctly', (done) => {
+    const service = createService();
+    service.isLoading$.subscribe(isLoading => {
+      expect(isLoading).toBe(false);
+      done();
+    });
+  });
+
+  it('should get access token correctly', (done) => {
+    const service = createService();
+    service.getAccessToken().subscribe(accessToken => {
+      expect(accessToken).toBe('test');
+      done();
+    });
+  });
+
+  it('should login correctly', async () => {
+    const service = createService();
+    await service.login();
+    expect(locationSpy.href).toBe('https://kinde.com/v2/login');
+  });
+
+  it('should logout correctly', async () => {
+    const service = createService();
+    await service.logout();
+    expect(locationSpy.href).toBe('https://kinde.com/v2/logout');
+  });
+
+  it('shouldHandleCallback returns if params exist', (done) => {
+    const service = createService();
+    const mockCallbackParams = {
+      code: 'testCode',
+      state: 'testState'
+    };
+    locationSpy.search = `?code=${mockCallbackParams.code}&state=${mockCallbackParams.state}`;
+
+    (service as any).shouldHandleCallback().subscribe((shouldHandle: boolean) => {
+      expect(shouldHandle).toBe(true);
+      done();
+    });
+  });
+
+  it('should not handle callback correctly if params exist', () => {
+    expect(kindeClientMock.handleRedirectToApp).not.toHaveBeenCalled()
+  });
+
+  it('should handle callback correctly if params exist', () => {
+    const mockCallbackParams = {
+      code: 'testCode',
+      state: 'testState'
+    };
+    locationSpy.search = `?code=${mockCallbackParams.code}&state=${mockCallbackParams.state}`;
+    createService();
+
+    expect(kindeClientMock.handleRedirectToApp).toHaveBeenCalled();
   });
 });
