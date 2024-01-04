7e94694b27b0f445802b0f0b3b73389ac210d78e
Luuk Haijes <luuk_haijes@yahoo.com>
2024-01-04 12:48:37 +0100

    Revert "Revert "fix: file paths were broken""
    This reverts commit b90ccbb17ac68a137e2fd0dff3b5e2414964eb01.

--- projects/playground/src/app/app-routing.module.ts
+++ projects/playground/src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { canActivateAuthGuard, canMatchAuthGuard } from "../../../kinde-angular/src/lib/auth.guard";
-import { BlaComponent } from "./bla.component";
+import { canActivateAuthGuard, canMatchAuthGuard, featureFlagGuard } from "../../../kinde-angular/src/lib/auth.guard";
+import { BlaComponent } from "./components/bla.component";
+import { FeatureComponent } from "./components/feature.component";
 
 const routes: Routes = [
   {
     path: 'bla',
     component: BlaComponent,
     canActivate: [canActivateAuthGuard]
+  },
+  {
+    path: 'feature',
+    component: FeatureComponent,
+    canActivate: [featureFlagGuard('has_feature')]
+  },
+  {
+    path: 'unknown-feature',
+    component: FeatureComponent,
+    canActivate: [featureFlagGuard('unknown_feature')]
   },
   {
     path: 'lazy',
